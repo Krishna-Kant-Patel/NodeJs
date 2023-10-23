1a3729dc04880703fc27a94efdd668f2b9264d77
Krishna <cu.17bcs1796@gmail.com>
2023-10-23 21:31:08 +0530

    :fire:

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const fs = require("fs");
+// fs.writeFileSync('read.txt','hello Krishna how are you');
+// fs.appendFileSync('read.txt',"I am fine just code as much as you can");
+
+
+// const buf_data = fs.readFileSync("read.txt");
+// data = buf_data.toString();
+// console.log(data);
+
+fs.renameSync("read.txt", "readWrite.txt");
