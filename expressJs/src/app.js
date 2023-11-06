9b933b851df55f5f29ffe89b7521a0e81766f068
Krishna <cu.17bcs1796@gmail.com>
2023-11-06 21:47:29 +0530

    :fire: static website hosted with epress js

--- expressJs/src/app.js
+++ expressJs/src/app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send("hello krishna how's doing")
+    res.send("hello krishna how's doing");
 });
 
 app.get("/about", (req, res)=>{
-    res.send("hello i am about page")
+    res.send("hello i am about page");
+});
+
+app.get("/contact", (req, res)=>{
+    res.send("welcome to contact page");
 })
 
 app.listen(8000, ()=>console.log("server started at 8000"))
--- expressJs/src/nav.js
+++ expressJs/src/nav.js
@@ -3,13 +3,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res)=>{
-    res.send("welcome to my home page");
+    res.send("<h1>welcome to my home page</h1>");
 });
 app.get("/about", (req, res)=>{
     res.send("welcome to my about page");
 });
 app.get("/contact", (req, res)=>{
-    res.send("welcome to my contact page");
+    res.json([{
+        id:1,
+        name:"krishna"
+    },
+{
+    id: 2,
+    name: "neeraj"
+}]);
 });
 
 app.listen(port, ()=>{
