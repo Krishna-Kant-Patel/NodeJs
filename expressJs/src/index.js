108fbfb4745c0bb1078d3648ea5aa05812f9a63d
Krishna <cu.17bcs1796@gmail.com>
2023-11-06 22:00:49 +0530

    :fire:

--- expressJs/src/index.js
+++ expressJs/src/index.js
@@ -4,7 +4,12 @@ const app = express();
 const port = 8000;
 
 const staticPath = path.join(__dirname,'../public');
-app.use(express.static(staticPath));
+app.set('view engine', "hbs");
+
+// app.use(express.static(staticPath));
+app.get("/", (req, res)=>{
+    res.render('index')
+})
 
 app.get("/", (req, res)=>{
     res.send("hello from server");
