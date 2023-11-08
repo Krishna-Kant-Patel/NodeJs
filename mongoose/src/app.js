406b393d35ef199096d0e3f37f9fec3a5805c551
Krishna <cu.17bcs1796@gmail.com>
2023-11-08 23:18:47 +0530

    :fire:

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -1,27 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Replace 'mydatabase' with your actual MongoDB database name
-const dbName = 'krishna';
-
-// Define the MongoDB connection URL
-const mongoURI = `mongodb://localhost:27017/${dbName}`;
-
-// Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Event handlers for MongoDB connection
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-
-  // Perform database operations here
-
-  // Close the connection when you're done
-  // mongoose.connection.close();
-});
+mongoose.connect("mongodb://127.0.0.1:27017/krishna",{useNewUrlParser: true})
+.then(()=> console.log("connection successful..."))
+.catch((error)=>console.log(error));
