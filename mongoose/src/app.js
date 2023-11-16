5b9abe5178ea7b898a94a6070c290de374711212
Krishna <cu.17bcs1796@gmail.com>
2023-11-16 21:56:19 +0530

    :fire: schema created

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/krishna",{useNewUrlParser: true})
+mongoose.connect("mongodb://127.0.0.1:27017/demo",{useNewUrlParser: true})
 .then(()=> console.log("connection successful..."))
 .catch((error)=>console.log(error));
+
+
+const krishna = new mongoose.Schema({
+    name: String,
+    tech: String,
+    age: Number,
+    active : Boolean
+})
+
+const Krish = new mongoose.model("demodata", krishna);
