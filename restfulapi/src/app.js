1c60caad1fec980f8b9c892c132715d767dadf65
Krishna <cu.17bcs1796@gmail.com>
2023-12-17 23:16:51 +0530

    :rocket:

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require('./db/conn');
-const LoginData = require('./models/login')
+const LoginData = require('./models/login');
+const Post = require('./models/posts');
+
+
 const port  = process.env.PORT || 8000;
+
+
+app.use(express.json({limit: '25mb'}));
+app.use(cors());
 
 app.get("/", async (req, res) => {
     try {
@@ -16,9 +23,6 @@ app.get("/", async (req, res) => {
     }
 })
 
-app.use(express.json());
-app.use(cors());
-
 app.post("/login", (req, res)=>{
     console.log(req.body);
     const user = new LoginData(req.body);
@@ -28,7 +32,30 @@ app.post("/login", (req, res)=>{
         res.send(e);
     })
     // res.send("response from other side");
+});
+
+app.get("/image", async (req, res) => {
+    try {
+        // Fetch data from MongoDB
+        const data = await Post.find();
+        
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
+app.use("/uploads", async (req, res) => {
+    const body = req.body;
+    try {
+      const newImage = await Post.create(body);
+      newImage.save();
+      res.status(201).json({message: "new image uploaded", createdPost: newImage});
+    } catch (error) {
+      res.status(409).json({
+        message: error.message,
+      });
+    }
+  });
 
 app.listen(port, ()=>{
     console.log(`connection set up at ${port}`);
